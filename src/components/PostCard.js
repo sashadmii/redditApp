8f9082588855d89983f599c7f1a95723e1229df2
sashadmii <alexandra.rychka@gmail.com>
2025-03-13 13:18:49 -0400

    CSS changes

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,71 +1,67 @@
 import './postCard.css';
 import {
-    loadComments,
-    selectComments,
+  loadComments,
+  selectComments,
 } from '../features/comments/commentsSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { Comments } from '../features/comments/Comments';
 import { useRef, useState, useEffect } from 'react';
 
 export const PostCard = ({
-    id,
-    author,
-    title,
-    selftext,
-    img,
-    postHint,
-    permalink,
+  id,
+  author,
+  title,
+  selftext,
+  img,
+  postHint,
+  permalink,
 }) => {
-    const dispatch = useDispatch();
-    const commentsToRenderObject = useSelector(selectComments);
-    const commentsToRender = commentsToRenderObject[id] || [];
-    console.info('commentsToRenderObject', commentsToRenderObject);
+  const dispatch = useDispatch();
+  const commentsToRenderObject = useSelector(selectComments);
+  const commentsToRender = commentsToRenderObject[id] || [];
+  console.info('commentsToRenderObject', commentsToRenderObject);
 
-    // on the title click redirects the user to the post on Reddit
-    const handleClick = (permalink) => {
-        window.open(
-            `https://www.reddit.com${permalink}`,
-            '_blank',
-            'noreferrer'
-        );
-    };
+  // on the title click redirects the user to the post on Reddit
+  const handleClick = (permalink) => {
+    window.open(`https://www.reddit.com${permalink}`, '_blank', 'noreferrer');
+  };
 
-    // expand / hide comments
-    const myRef = useRef(null);
-    const [visible, setVisible] = useState();
+  // expand / hide comments
+  const myRef = useRef(null);
+  const [visible, setVisible] = useState();
 
-    useEffect(() => {
-        myRef.current.addEventListener('click', setVisibility);
-    });
+  useEffect(() => {
+    myRef.current.addEventListener('click', setVisibility);
+  });
 
-    const setVisibility = () => setVisible(!visible);
+  const setVisibility = () => setVisible(!visible);
 
-    return (
-        <div className="card" key={id}>
-            {postHint === 'image' && <img alt={title} src={img} />}
-            <h3>{author}</h3>
-            <h2 role="link" onClick={() => handleClick(permalink)}>
-                {title}
-            </h2>
-            <p className="selftext">{selftext}</p>
+  return (
+    <div className="card" key={id}>
+      {postHint === 'image' && <img alt={title} src={img} />}
+      <h3>{author}</h3>
+      <h2 role="link" onClick={() => handleClick(permalink)}>
+        {title}
+      </h2>
+      <p className="selftext">{selftext}</p>
 
-            <button
-                className="commentsButton"
-                ref={myRef}
-                onClick={() => dispatch(loadComments(permalink))}>
-                {visible ? 'Hide comments' : 'Show comments'}
-            </button>
-            {visible && (
-                <div className="commentsContainer">
-                    {commentsToRender.map((comment) => (
-                        <Comments
-                            key={comment.id}
-                            author={comment.author}
-                            text={comment.body}
-                        />
-                    ))}
-                </div>
-            )}
+      <button
+        className="commentsButton"
+        ref={myRef}
+        onClick={() => dispatch(loadComments(permalink))}>
+        {visible ? 'Hide comments' : 'Show comments'}
+      </button>
+      {visible && (
+        <div className="commentsContainer">
+          {commentsToRender.map((comment) => (
+            <Comments
+              key={comment.id}
+              author={comment.author}
+              text={comment.body}
+            />
+          ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
--- src/features/searchBar/SearchBar.js
+++ src/features/searchBar/SearchBar.js
@@ -3,43 +3,43 @@ import './SearchBar.css';
 import SearchIcon from '@mui/icons-material/Search';
 import { setSearch } from './searchSlice';
 import { useDispatch } from 'react-redux';
-import logo from '../../icon/pngegg.svg';
+import logo from '../../icon/reddit.svg';
 
 export const SearchBar = ({ placeholder }) => {
-    const dispatch = useDispatch();
-    const myRef = useRef(null);
+  const dispatch = useDispatch();
+  const myRef = useRef(null);
 
-    const [userSearch, setUserSearch] = useState('');
+  const [userSearch, setUserSearch] = useState('');
 
-    // input works, when the user hits 'enter'
-    useEffect(() => {
-        myRef.current.addEventListener('keypress', function (event) {
-            if (event.key === 'Enter') {
-                event.preventDefault();
-                document.getElementById('buttonSearch').click();
-                console.log('click>>>', userSearch);
-            }
-        });
+  useEffect(() => {
+    myRef.current.addEventListener('keypress', function (event) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('buttonSearch').click();
+      }
     });
+  });
 
-    return (
-        <div className="search">
-            <img alt="reddit_icon" src={logo} />
-            <input
-                type="text"
-                placeholder={placeholder}
-                value={userSearch}
-                onChange={(e) => setUserSearch(e.target.value)}
-                ref={myRef}
-            />
-            <button
-                id="buttonSearch"
-                className="searchButton"
-                onClick={() => {
-                    dispatch(setSearch(userSearch));
-                }}>
-                <SearchIcon />
-            </button>
-        </div>
-    );
+  return (
+    <div className="search">
+      <img alt="reddit_icon" src={logo} />
+      <div className="searchInput">
+        <input
+          type="text"
+          placeholder="What you are looking for?"
+          value={userSearch}
+          onChange={(e) => setUserSearch(e.target.value)}
+          ref={myRef}
+        />
+        <button
+          id="buttonSearch"
+          className="searchButton"
+          onClick={() => {
+            dispatch(setSearch(userSearch));
+          }}>
+          <SearchIcon />
+        </button>
+      </div>
+    </div>
+  );
 };
